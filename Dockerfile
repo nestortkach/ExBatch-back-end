80dff5ac2ec84eefc364402fc6f48137c9fa4acc
Oleksii <oleksii.oliinyk.work@gmail.com>
2025-05-13 13:28:41 +0300

    trying fix db

--- Dockerfile
+++ Dockerfile
@@ -12,10 +12,10 @@ RUN pip install --no-cache-dir -r requirements.txt
 
 COPY . .
 
-ENV PYTHONDONTWRITEBYTECODE=1  \
-    PYTHONUNBUFFERED=1         \
+ENV PYTHONDONTWRITEBYTECODE=1 \
+    PYTHONUNBUFFERED=1 \
     PORT=8000
 
-EXPOSE ${PORT}
+EXPOSE 8000
 
-CMD ["uvicorn", "app.main:app", "--host", "0.0.0.0", "--port", "8000"]
+CMD ["sh", "-c", "uvicorn app.main:app --host 0.0.0.0 --port 8000"]
--- app/settings.py
+++ app/settings.py
@@ -4,7 +4,6 @@ from dotenv import load_dotenv
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 STORAGE_DIR = BASE_DIR / "storage"
-DB_FILE = BASE_DIR / "batcher.db"
 ENV_PATH = BASE_DIR / '.env'
 
 STORAGE_DIR.mkdir(parents=True, exist_ok=True)
@@ -18,6 +17,6 @@ DATABASES = {
         'USER': os.environ.get('POSTGRES_USER'),
         'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
         'HOST': os.environ.get('POSTGRES_HOST'),
-        'PORT': os.environ.get('POSTGRES_PORT', '5432'),
+        'PORT': os.environ.get('POSTGRES_PORT'),
     }
 }
