89aab42234881a3147e34dc398ff919a9e0fcb16
Oleksii <oleksii.oliinyk.work@gmail.com>
2025-05-15 15:51:14 +0300

    db url change

--- app/models.py
+++ app/models.py
@@ -15,8 +15,8 @@ POSTGRES_HOST = os.getenv("POSTGRES_HOST")
 POSTGRES_PORT = os.getenv("POSTGRES_PORT")
 
 
-DATABASE_URL = "postgresql://test_db_user:1234AAA@localhost:5432/Ex_batch"
-#DATABASE_URL = f"postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_HOST}:{POSTGRES_PORT}/{POSTGRES_DB}"
+#DATABASE_URL = "postgresql://test_db_user:1234AAA@localhost:5432/Ex_batch"
+DATABASE_URL = f"postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_HOST}:{POSTGRES_PORT}/{POSTGRES_DB}"
 
 engine = create_engine(DATABASE_URL)
 
