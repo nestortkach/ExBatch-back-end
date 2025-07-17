d19357e95a2bf7b786c16b00611b2ace2e16cec2
nestortkack <nestor.tkach17@gmail.com>
2025-07-17 15:58:37 +0300

    added irr func

--- app/services/excel_processor.py
+++ app/services/excel_processor.py
@@ -1,10 +1,25 @@
+import tempfile
 import openpyxl
+import numpy_financial as npf
 import csv, re, uuid, io
 from pathlib import Path
 from io import BytesIO
 from app.settings import STORAGE_DIR
 from app.schemas import TemplateCreate
 from xlcalculator import ModelCompiler, Evaluator
+
+
+def calculate_irr(values, guess=None):
+    return npf.irr(values)
+
+def parse_cell_reference(cell):
+    pattern = r'^([A-Z]+)(\d+)$'
+    match = re.match(pattern, cell)
+    if match:
+        column, row = match.groups()
+        return [column, int(row)] 
+    else:
+        raise ValueError(f"Invalid format of the cell: {cell}")
 
 _CELL_RGX = re.compile(r"([A-Z]+)(\d+)", re.I)
 def _split(cell: str) -> tuple[str, int]:
@@ -34,6 +49,13 @@ def process_excel_file(
     for idm in template.identity_mappings:
         id_maps.append(idm.name)
 
+    with tempfile.NamedTemporaryFile(delete=False, suffix='.xlsx') as tmp:
+        tmp.write(file_bytes)
+        tmp_path = tmp.name
+
+    wb = openpyxl.load_workbook(tmp_path,data_only=True, keep_vba=True)
+    workspace = openpyxl.load_workbook(BytesIO(file_bytes))
+
     mc = ModelCompiler()
     model = mc.read_and_parse_archive(BytesIO(file_bytes))
     ev = Evaluator(model)
@@ -55,13 +77,34 @@ def process_excel_file(
             out_row = {}
             for sh, col, r, field in out_maps:
                 formula_cell = f"{sh}!{col}{r}"
+
+                workspace_sheet = workspace[sh]
+                copy_workspace_sheet = wb[sh]
+                
                 try:
-                    result = ev.evaluate(formula_cell)
+                    curr_cell = workspace_sheet[col+str(r)].value
+                    if ref_cells := re.search(r"[A-Za-z]\d+", curr_cell):
+                        ref_cell = workspace_sheet[ref_cells.group(0)].value
+                        if "IRR" in ref_cell: 
+                            list_cells = re.findall(r'([A-Z]+[0-9]+):([A-Z]+[0-9]+)', ref_cell)
+                            
+                            print()
+
+                            first_num_list = parse_cell_reference(list_cells[0][0])
+                            second_num_list = parse_cell_reference(list_cells[0][1])
+
+                            list_cell = []
+                            for indx in range(first_num_list[1], second_num_list[1]):
+                                list_cell.append(
+                                    copy_workspace_sheet[first_num_list[0] + str(indx)].value
+                                )
+                            result = calculate_irr(list_cell)
+                        else:              
+                            result = ev.evaluate(formula_cell)
                     out_row[field] = result or 0
                     
                 except Exception as e:
                     raise ValueError(f"Formula calculation error in cell {formula_cell}. {str(e)}")
-
             identity_data = {key: csv_row.get(key, "") for key in id_maps}
 
             combined_row = {**identity_data, **out_row}
